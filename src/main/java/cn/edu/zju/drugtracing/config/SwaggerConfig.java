c28e6fc9c67ece5d57f3128eda9836e7cc4f5682
zjxjwxk <zjxjwxk@gmail.com>
2020-08-18 10:23:19 +0800

    change package path

--- src/main/java/cn/edu/zju/drugtracing/config/SwaggerConfig.java
+++ src/main/java/cn/edu/zju/drugtracing/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("cn.edu.zju.drugtracing.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
